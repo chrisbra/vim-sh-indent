c9b2fce7f0eee1011032348134d571d4cedb4792
Christian Brabandt <cb@256bit.org>
2019-03-25 08:06:22 +0100

    Test: fix various small problems

--- test/test.sh
+++ test/test.sh
@@ -6,20 +6,24 @@
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 reset=$(tput sgr0)
+rc=0
 
 for i in */; do
-  (
+  {
     cd "$i" &&
     test -f "cmd.sh" &&
     ./cmd.sh
     diff -qu output.sh  reference.sh >/dev/null
     if [ $? -ne 0 ]; then
       printf "Test %s:\t\t[${red}Failed${reset}]\n" ${i%%/}
-      if [ -n "$1" ] && [ "${1}" -eq "-v" ]; then
-        diff -u output.xml reference.xml
+      if [ -n "$1" ] && [ "${1}" = "-v" ]; then
+        diff -u --color output.sh reference.sh
       fi
+      rc=1
     else
       printf "Test %s:\t\t[${green}OK${reset}]\n" ${i%%/}
     fi
-    )
+    cd "$OLDPWD"
+  }
 done
+exit $rc
