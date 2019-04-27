chrisbra/vim-sh-indent

commit ef2e050b690bab58ed3d3dfd51147f9bcd2cf870

Better indenting for nested if sections
closed #10 #11 #12

indent/sh.vim
@@ -7,6 +7,8 @@
 " License:             Vim (see :h license)
 " Repository:          https://github.com/chrisbra/vim-sh-indent
 " Changelog:
+"          20190316  - Make use of searchpairpos for nested if sections
+"                      fixes #11
 "          20190201  - Better check for closing if sections
 "          20180724  - make check for zsh syntax more rigid (needs word-boundaries)
 "          20180326  - better support for line continuation
@@ -115,7 +117,7 @@ function! GetShIndent()
   " Current line is a endif line, so get indent from start of "if condition" line
   " TODO: should we do the same for other "end" lines?
   if curline =~ '^\s*\%(fi\)\s*\%(#.*\)\=$'
-    let previous_line = search('if.\{-\};\s*then\s*\%(#.*\)\=$', 'bnW')
+    let previous_line = searchpair('\<if\>', '', '\<fi\>', 'bnW')
     if previous_line > 0
       let ind = indent(previous_line)
     endif

test/02/cmd.sh
@@ -1,0 +1,10 @@
+#!/bin/sh
+
+vim --clean \
+    -c ':unlet! b:did_indent' \
+    -c ':delfunc! GetShIndent' \
+    -c ':so ../../indent/sh.vim' \
+    -c ':set sw=0 sts=-1 ts=2 et' \
+    -c 'norm! gg=G' \
+    -c ':saveas! output.sh' \
+    -c ':q!' indent.sh

test/02/indent.sh
@@ -1,0 +1,24 @@
+#!/bin/sh
+
+if [[ /bin/true ]]; then
+        echo "true"
+else
+        echo "false"
+        if [[ /bin/false ]]; then
+                echo "false"
+        else
+                echo "true"
+        fi
+        fi
+        if [[ /bin/true ]]
+        then
+                echo "true"
+        else
+                echo "false"
+                if [[ /bin/false ]]
+                then
+                        echo "false"
+                else
+                        echo "true"
+        fi
+        fi

test/02/reference.sh
@@ -1,0 +1,24 @@
+#!/bin/sh
+
+if [[ /bin/true ]]; then
+  echo "true"
+else
+  echo "false"
+  if [[ /bin/false ]]; then
+    echo "false"
+  else
+    echo "true"
+  fi
+fi
+if [[ /bin/true ]]
+then
+  echo "true"
+else
+  echo "false"
+  if [[ /bin/false ]]
+  then
+    echo "false"
+  else
+    echo "true"
+  fi
+fi

test/03/cmd.sh
@@ -1,0 +1,10 @@
+#!/bin/sh
+
+vim --clean \
+    -c ':unlet! b:did_indent' \
+    -c ':delfunc! GetShIndent' \
+    -c ':so ../../indent/sh.vim' \
+    -c ':set sw=0 sts=-1 ts=2 et' \
+    -c 'norm! gg=G' \
+    -c ':saveas! output.sh' \
+    -c ':q!' indent.sh

test/03/indent.sh
@@ -1,0 +1,13 @@
+#/bin/sh
+
+if true; then
+    echo 'pipe beginning'
+        sed 's/beginn/end/'
+fi
+
+# TODO:
+# The line after the pipe will be indented. Is this correct?
+if true; then
+    echo 'pipe beginning' |
+        sed 's/beginn/end/'
+fi

test/03/reference.sh
@@ -1,0 +1,13 @@
+#/bin/sh
+
+if true; then
+  echo 'pipe beginning'
+  sed 's/beginn/end/'
+fi
+
+# TODO:
+# The line after the pipe will be indented. Is this correct?
+if true; then
+  echo 'pipe beginning' |
+    sed 's/beginn/end/'
+fi

commit a85ccac0c292f2d13709b763565b8e4375918fc2

de-indent correctly after typing 'fi'
Regression from ef2e050 "Better indenting for nested if sections" which
changed to using searchpair() instead of search().

When typing "fi", the cursor is after the start of the \<fi\> match, so
vim un-indents back to the next outer if/fi block rather than the
expected block.

The help for searchpair() suggests "When searching backwards and {end} is
more than one character, it may be useful to put "\zs" at the end of the
pattern, so that when the cursor is inside a match with the end it finds
the matching start." Follow this suggestion to restore the expected
behavior.

Fixes: https://github.com/chrisbra/vim-sh-indent/issues/15

indent/sh.vim
@@ -3,10 +3,12 @@
 " Maintainer:          Christian Brabandt <cb@256bit.org>
 " Original Author:     Nikolai Weibull <now@bitwi.se>
 " Previous Maintainer: Peter Aronoff <telemachus@arpinum.org>
-" Latest Revision:     2019-03-25
+" Latest Revision:     2019-04-27
 " License:             Vim (see :h license)
 " Repository:          https://github.com/chrisbra/vim-sh-indent
 " Changelog:
+"          20190427  - De-indent fi correctly when typing with
+"                      https://github.com/chrisbra/vim-sh-indent/issues/15
 "          20190325  - Indent fi; correctly
 "                      https://github.com/chrisbra/vim-sh-indent/issues/14
 "          20190319  - Indent arrays (only zsh and bash)
@@ -127,7 +129,7 @@ function! GetShIndent()
   " Current line is a endif line, so get indent from start of "if condition" line
   " TODO: should we do the same for other "end" lines?
   if curline =~ '^\s*\%(fi\);\?\s*\%(#.*\)\=$'
-    let previous_line = searchpair('\<if\>', '', '\<fi\>', 'bnW')
+    let previous_line = searchpair('\<if\>', '', '\<fi\>\zs', 'bnW')
     if previous_line > 0
       let ind = indent(previous_line)
     endif