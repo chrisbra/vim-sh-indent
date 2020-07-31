c6e0d5f6a3cc4aea84da52a4f69974b1e01df892
Wayne Walker <wwalker@solid-constructs.com>
2020-07-30 21:49:13 -0500

    Fix issue #27 - local breaks array indention

---
+++ test/16/indent.sh
@@ -0,0 +1,26 @@
+ste(){
+local attrs=(
+"asdf"
+"asdf2"
+"asdf3"
+)
+}
+
+ste(){
+attrs=(
+"asdf"
+# asdf
+"asdf2"
+                 # asdf
+"asdf3"
+)
+}
+
+ste(){
+local attrs
+attrs=(
+"asdf"
+"asdf2"
+"asdf3"
+)
+}
