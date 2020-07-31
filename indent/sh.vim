chrisbra/vim-sh-indent

commit 6945e9153f92dfb51262c0a9406ad79da4828884

Fix line indent after a last continuation line
- Let's assume a situation where there is a command right after the last
  continuation line. In that case this line with a command would have an
  indentation from the previous line, which is the last continuation
  line. With this commit the line with a command after last continuation
  line would have the same an indentation as the line before the first
  continuation line.

- Add a test for this commit.

For example, the following code snippet

```sh
echo heavy metal |
                 cat
                 echo bulldozer
```

with this commit would be

```sh
echo heavy metal |
                 cat
echo bulldozer
```

indent/sh.vim
@@ -120,7 +120,7 @@ function! GetShIndent()
     while !s:is_empty(getline(i)) && i > pnum
       let i -= 1
     endw
-    if i == pnum
+    if i == pnum && s:is_continuation_line(line)
       let ind += ind2
     else
       let ind = ind2

test/14/cmd.sh
@@ -1,0 +1,10 @@
+#!/bin/sh
+
+vim --clean \
+    -c ':unlet! b:did_indent' \
+    -c ':delfunc! GetShIndent' \
+    -c ':so ../../indent/sh.vim' \
+    -c ':set sw=0 sts=-1 ts=2 et' \
+    -c 'norm! gg=G' \
+    -c ':saveas! output.sh' \
+    -c ':q!' indent.sh

test/14/indent.sh
@@ -1,0 +1,5 @@
+#!/bin/sh
+
+a |
+  b
+c

test/14/reference.sh
@@ -1,0 +1,5 @@
+#!/bin/sh
+
+a |
+  b
+c

commit c37bde65f1218427feea164f0a1006516312746a

Fix issue #33 - Test 01 fails
In addition to the `line` also check `pline` for '{'.

Fixes: 6945e915 ("Fix line indent after a last continuation line")

indent/sh.vim
@@ -120,7 +120,7 @@ function! GetShIndent()
     while !s:is_empty(getline(i)) && i > pnum
       let i -= 1
     endw
-    if i == pnum && s:is_continuation_line(line)
+    if i == pnum && (s:is_continuation_line(line) || pline =~ '{\s*\(#.*\)\=$')
       let ind += ind2
     else
       let ind = ind2