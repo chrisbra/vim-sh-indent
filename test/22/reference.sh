982fbbc34f9c66af1f6b58b643d0f4ef32b7d8c8
Andreas Höhne <info@webdesign-hoehne.de>
2025-03-18 22:33:42 +0100

    Fix indent for local arrays, also fixed arrays set with typeset and declare prefix (#46)
    * fix indentation for local arrays
    
    * fix indentation for arrays with declare and typeset prefix

---
+++ test/22/reference.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# vim: set ft=bash sw=2 noet:
+
+function variables(){
+  local test1="test1"
+  test2=(
+    "test" "test2"
+  )
+  local test3=(
+    "test" "test3"
+  )
+  typeset -ua test4=(
+    "test" "test4"
+  )
+  declare -a inline=("also work") multiline=(
+    "multiple" "values"
+  )
+  declare -l var="VALUE"
+}
